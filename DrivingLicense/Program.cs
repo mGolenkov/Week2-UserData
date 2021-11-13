30feb5f3f0c0e67efffb53890d713bac72b3bfb5
Maksim Golenkov <mgolenko@tktk.ee>
2021-11-13 16:14:05 +0200

    Control Flow

---
+++ DrivingLicense/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace DrivingLicense
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //programm küsib sünniaastat
+            //programm arvutab kasutaja vanust
+            //kui kasutaja on noorem, kui 18, siis programm kuvab konsoolis
+            //"oled liiga noor, et juhilube saada";
+            //kui kasutaja on vanem, kui 18, siis program kuvab konsoolis
+            //"oled piisavlt vana, et juhi lube saada";
+            //kui kasutaja on 18, siis programm kuvab konsoolis
+            //"Palju õnne! Nüüd sa saad juhilube taotleda";
+
+            Console.WriteLine("Palun, siseta oma sünniaasta:");
+            int yearOfBirth = Convert.ToInt32(Console.ReadLine());
+            
+            int userAge = 2021 - yearOfBirth;
+
+            if (userAge < 18)
+            {
+                Console.WriteLine("Oled liiga noor, et juhilube saada");
+            }
+            else if (userAge > 18)
+            {
+                Console.WriteLine("Oled piisavalt vana, et juhilube saada");
+            }
+            else
+            {
+                Console.WriteLine("Palju õnne! Nüüd sa saad juhilube taotleda");
+            }
+        }
+    }
+}
---
+++ UserData/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace UserData
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //programm küsib katutaja sünniaastat;
+            //programm küsib kasutaja vanust;
+            //programm kuvab kastutaja vanust konsoolis;
+
+            Console.WriteLine("Palun, sisesta oma sünniaasta:");
+            int yearOfBirth = Convert.ToInt32(Console.ReadLine());
+
+            int userAge = 2021 - yearOfBirth;
+
+            //Console.WriteLine("Oled " + userAge + " aastatvana.");
+
+            //string interpolation
+
+            Console.WriteLine($"Oled {userAge} aastat vana.");
+
+
+
+
+
+
+        }
+    }
+}
